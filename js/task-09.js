45753a875136ec36f6d8255ded53bfeb236df877
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-04-17 21:42:52 +0300

    Create task-09.js

---
+++ js/task-09.js
@@ -0,0 +1,22 @@
+
+
+const widget = document.querySelector('.widget');
+const colorSpan = widget.querySelector('.color');
+const changeColorBtn = widget.querySelector('.change-color');
+
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+changeColorBtn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
