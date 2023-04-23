775afe5c9c83d386aacd08a5d152f93badf911b6
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-04-23 20:25:02 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,8 +2,11 @@ const changeControl = document.getElementById("font-size-control");
 const text = document.getElementById("text");
 
 
+const onStartFontSize = changeControl.value;
+text.style.fontSize = `${onStartFontSize}px`;
 
-changeControl.addEventListener("input",   ()=> {
+
+changeControl.addEventListener("input", () => {
 
     text.style.fontSize = `${changeControl.value}px`;
     console.log(text.style.fontSize)
