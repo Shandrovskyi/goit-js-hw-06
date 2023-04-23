2c595c737327c0026b6f1c805399365211e4bc39
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-04-23 20:25:06 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,24 @@
-
-
-const widget = document.querySelector('.widget');
-const colorSpan = widget.querySelector('.color');
-const changeColorBtn = widget.querySelector('.change-color');
-
+// const widget = document.querySelector('.widget');
+// const colorSpan = widget.querySelector('.color');
+// const changeColorBtn = widget.querySelector('.change-color');
 
 
 
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
+
+// changeColorBtn.addEventListener('click', () => {
+//   const newColor = getRandomHexColor();
+//   document.body.style.backgroundColor = newColor;
+//   colorSpan.textContent = newColor;
+// });
+
+
+// =================================NEW======================
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -15,8 +26,16 @@ function getRandomHexColor() {
 }
 
 
+const changeColorBtn = document.querySelector('.change-color');
+const colorChange = document.querySelector('.color');
+const body = document.querySelector('body');
+
+
+
+
 changeColorBtn.addEventListener('click', () => {
-  const newColor = getRandomHexColor();
-  document.body.style.backgroundColor = newColor;
-  colorSpan.textContent = newColor;
+
+  colorChange.textContent = getRandomHexColor();
+  body.style.backgroundColor = colorChange.textContent;
+
 });
