56335f3b1ff3e68b3863eb4cb93f1120d8469fc9
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-04-23 20:25:04 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,45 @@
-const loginForm = document.querySelector('.login-form');
+// const loginForm = document.querySelector('.login-form');
 
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault(); 
+// loginForm.addEventListener('submit', (event) => {
+//   event.preventDefault(); 
 
-  const formData = new FormData(loginForm); 
-  const data = {}; 
+//   const formData = new FormData(loginForm); 
+//   const data = {}; 
 
-  for (let [name, value] of formData) {
-    if (!value) { 
-      alert('всі поля повинні бути заповнені'); 
-      return; 
-    }
+//   for (let [name, value] of formData) {
+//     if (!value) { 
+//       alert('всі поля повинні бути заповнені'); 
+//       return; 
+//     }
 
-    data[name] = value; 
+//     data[name] = value; 
+//   }
+
+//   console.log(data); 
+//   loginForm.reset(); 
+// });
+
+
+// ================================NEW=========================
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const emailInput = form.elements.email;
+  const passwordInput = form.elements.password;
+
+  if (emailInput.value === '' || passwordInput.value === '') {
+    alert('всі поля повинні бути заповнені');
+    return;
   }
 
-  console.log(data); 
-  loginForm.reset(); 
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+  form.reset();
 });
