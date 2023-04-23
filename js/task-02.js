3a5bb7c0170667360aa01598fe53d2233f0fce83
Shandrovskyi <119674969+Shandrovskyi@users.noreply.github.com>
2023-04-23 20:24:57 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,23 +7,49 @@
 // 4.Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
 
 
+// ================================OLD=================================
 
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+
+// const ul = document.querySelector("#ingredients");
+
+// for( const ingredient of ingredients){
+
+//   const li = document.createElement("LI");
+//   li.textContent = ingredient;
+//   ul.appendChild(li);
+// }
+
+
+
+// ==================================NEW==================================
 
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
 
 const ul = document.querySelector("#ingredients");
 
-for( const ingredient of ingredients){
+const product = ingredients.map((ingredient) => {
+const li = document.createElement("li");
 
-  const li = document.createElement("LI");
-  li.textContent = ingredient;
-  ul.appendChild(li);
-}
+li.textContent = ingredient;
+return li;
+
+});
+
+ul.append(...product);
